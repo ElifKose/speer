b87a0af545d97ae10475b00c9d0b812d5d3b64f1
Elif Kose <kose.elif@hotmail.com>
2023-02-25 12:23:15 -0500

    Initial commit

---
+++ src/test/java/Pages/BasePage.java
@@ -0,0 +1,9 @@
+package Pages;
+
+import Core.Driver;
+import org.openqa.selenium.WebDriver;
+
+public abstract class BasePage {
+
+    public static WebDriver driver = Driver.getDriver();
+}
---
+++ src/test/java/StepDefinitions/Hooks.java
@@ -0,0 +1,21 @@
+package StepDefinitions;
+
+import Core.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+public class Hooks {
+    private WebDriver driver;
+
+    @After("not @hookless")
+    public void tearDown(Scenario scenario) {
+        if (scenario.isFailed()) {
+            byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", scenario.getName());
+        }
+    }
+
+}
